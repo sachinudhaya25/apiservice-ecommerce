91c26d8912b72654764bac079cfad120c07f6aaa
sachin <nudhayaraj99@gmail.com>
2022-05-05 10:49:55 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/apiservice/ApiServiceApplication.java
@@ -0,0 +1,34 @@
+package com.ecommerce.apiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class ApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiceApplication.class, args);
+	}
+	@Bean
+    public Docket swaggerConfiguration() { 
+	        return new Docket(DocumentationType.SWAGGER_2)  
+	          .select()                                  
+	          .apis(RequestHandlerSelectors.basePackage("com.ecommerce"))              
+	          .paths(PathSelectors.any())                          
+	          .build();                                           
+	 }
+	@Bean
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
+	}
+}
